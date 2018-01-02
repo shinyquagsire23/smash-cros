7b6442c2182431bd241163a118a4cd2ac9bcf968
shinyquagsire23 <mtinc2@gmail.com>
2018-01-02 01:55:12 -0700

    mario_test: Update test sample

--- mario_test/source/main.c
+++ mario_test/source/main.c
@@ -3,15 +3,39 @@
 #include "CommonTranslated_fighter.h"
 
 extern void* _Z8NewmarioPv_orig(CommonTranslated_fighter* fighter);
+extern int _ZN3MSC32TranslatedMotionScriptEngineCore9fastshiftEi(CommonTranslated_fighter* fighter, void* func);
 
 // We're intentionally avoiding .bss here, .bss is extremely touchy at the moment
 void* vtable_test[44] = {-1};
 void* intercept_funbox_init_ptr = -1;
 void* original_funbox_init_ptr = -1;
+void* original_677 = -1;
+
+int perframe(CommonTranslated_fighter* fighter)
+{
+   fighter->msc_state->unk_0_variable_related->moduleAccessor->workModuleImpl->vtable->variable_fadd(fighter->msc_state->unk_0_variable_related->moduleAccessor->workModuleImpl, 0x1000004, 123.0f);
+
+   float test = fighter->msc_state->unk_0_variable_related->moduleAccessor->workModuleImpl->vtable->variable_fget(fighter->msc_state->unk_0_variable_related->moduleAccessor->workModuleImpl, 0x1000004);
+   printf("perframe! %f", test);
+   for (int i = 0; i < 10; i++)
+      printf("%x", fighter->msc_state->unk_0_variable_related->unk_0[i]);
+
+   return 0;
+}
 
 int nullsub(CommonTranslated_fighter* fighter)
 {
-   printf("nullsub!");
+   printf("nullsub! %x", fighter->stick_y_digital);
+   /*for (int i = 0; i < 0x44; i++)
+   {
+      printf("CommonTranslated_fighter %x: %08x", i*4, ((void**)fighter)[i]);
+   }*/
+   
+   fighter->funbox_every_frame_2 = perframe;
+   
+   //void* (*func)(void *arg1) = (void*)original_677;
+   //func(fighter);
+   //_ZN3MSC32TranslatedMotionScriptEngineCore9fastshiftEi(fighter, original_677);
 
    return 0;
 }
@@ -37,6 +61,7 @@ void intercept_init(CommonTranslated_fighter* fighter)
       ((void**)fighter)[i+722] = nullsub;
    }*/
    
+   original_677 = fighter->vtable_677;
    fighter->vtable_677 = nullsub; // Double jump or something
    
    /*for (int i = 0; i < 0x9f7; i++)
